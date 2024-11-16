f61bb38db4b88ba4b81199410c0271d05a011127
sujalshrestha <sujalshresthawork@gmail.com>
2024-11-16 22:51:47 +0545

    content model

--- src/models/content.model.ts
+++ src/models/content.model.ts
@@ -1,2 +1,21 @@
 import mongoose from 'mongoose';
-const contentSchema = new mongoose.Schema({});
+const contentSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ['youtube', 'tweet', 'document', 'link'],
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+    },
+});
+export const Content = mongoose.model('Content', contentSchema);
