66a2017831aaa3f6f367596a4cab1d0949e80440
sujalshrestha <sujalshresthawork@gmail.com>
2024-12-02 20:53:24 +0545

    prod

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import app from './app';
 import connectDB from './db';
 
-connectDB()
-    .then(() => {
-        app.listen(3000, () => console.log('Server running on port 3000'));
-    })
-    .catch((e) => {
-        console.error(`Error: ${(e as Error).message}`);
-        process.exit(1);
-    });
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+    connectDB();
+});
+export default app;
